bf38cf706bf8780d3e6691e8587e5ee8e64ad151
sreepriya73 <sreepriyaillikkatt@gmail.com>
2024-06-07 15:03:26 +0530

    compltd

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-success">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">BOOK APP</a>
+    <Link class="navbar-brand" to="#">BOOK APP</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">ADD</a>
+          <Link class="nav-link active" aria-current="page" to="/">ADD</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/search">SEARCH</a>
+          <Link class="nav-link" to="/search">SEARCH</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/delete">DELETE</a>
+          <Link class="nav-link" to="/delete">DELETE</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/view ">VIEW</a>
+          <Link class="nav-link" to="/view ">VIEW</Link>
         </li>
       </ul>
     </div>
