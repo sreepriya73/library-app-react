3800072bc9d375824f5e6dc3d21af1e092f4fb2c
sreepriya73 <sreepriyaillikkatt@gmail.com>
2024-06-07 13:01:28 +0530

    delete complted

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddBook from './component/AddBook';
 import SearchBook from './component/SearchBook';
+import DeleteBook from './component/DeleteBook';
 
 function App() {
   return (
     <div>
       <AddBook/>
       <SearchBook/>
+      <DeleteBook/>
     </div>
   );
 }
